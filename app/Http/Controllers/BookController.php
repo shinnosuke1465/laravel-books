63e6db60ac1618d44fcd91e639b8d3b19f5187e5
Shinnosuke1465 <s.shinno0605@icloud.com>
2023-08-18 20:37:42 +0900

    記事の詳細ページまで作成

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -12,9 +12,21 @@ class BookController extends Controller
     public function index()
     {
         //examplesテーブルから全てのデータを取得して変数に格納
-        $books = Book::all();
+        // $books = Book::all();
+        $book = Book::paginate(10);
 
         //DBから取得した値をcontrollerからviewに渡す(view/bookのbooks変数に$booksを配列で渡す)
-        return view('book.index', ['books' => $books]);
+        return view('book.index', ['books' => $book]);
     }
+
+    //routerからurlで飛んできたidをうけとる
+    public function detail($id)
+    {
+
+        $books = Book::findOrFail($id);
+
+        //DBから取得した値をcontrollerからviewに渡す(view/bookのbooks変数に$booksを配列で渡す)
+        return view('book.detail', ['book' => $books]);
+    }
+
 }
